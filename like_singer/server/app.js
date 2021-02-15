cbcf0569608d01df9a3a3ac19803424cd5c54dd4
josellia <joseliaapires@hotmail.com>
2021-02-15 18:32:58 -0300

    refactor: refatorando a rota

--- like_singer/server/app.js
+++ like_singer/server/app.js
@@ -1,10 +1,10 @@
 import express  from 'express';
-import IndexRoutes from './routes/index';
+import SingerRoutes from './routes/singer.routes';
 
 const app = express();
 
 app.set('port', process.env.PORT ||  3000);
 
-app.use(IndexRoutes);
+app.use('/api',SingerRoutes);
 
 export default app;
