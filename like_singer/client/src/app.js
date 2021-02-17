5916be735c1d550626b754feb46a3d50446fd8a9
josellia <joseliaapires@hotmail.com>
2021-02-17 18:22:01 -0300

    refactor: adequando ao javascipt puro

--- like_singer/client/src/app.js
+++ like_singer/client/src/app.js
@@ -1,3 +1,24 @@
+let singers = [];
+//Datas
+async function getSingers(){
+    let URI = "http://localhost:3000/api/singers";
+    try {
+        let res = await fetch(URI);
+        singers = await res.json();
+        console.log(singers);
+    }catch(error){
+        console.log(console.error);
+    }  
+}
+
+// function postSinger(singer){
+//     const res = await fetch(URI, {
+//         method: 'POST',
+//         body: singer
+//     });
+//     const data = res.json();
+//     return data;
+// }
 document.querySelector('#singer-form')
 .addEventListener('submit', function(e) {
     e.preventDefault();
@@ -5,3 +26,9 @@ document.querySelector('#singer-form')
     const status = document.querySelector('#status').value;
     
 });
+
+async function init(){
+    await getSingers();
+}
+
+init();
--- like_singer/server/app.js
+++ like_singer/server/app.js
@@ -1,12 +1,13 @@
 import express  from 'express';
 import SingerRoutes from './routes/singer.routes';
+const cors = require('cors');
 
 const app = express();
 
 app.set('port', process.env.PORT ||  3000);
 
 app.use(express.json())
-
+app.use(cors());
 app.use('/api/singers',SingerRoutes);
 
 export default app;
