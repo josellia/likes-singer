25c9a135688400a35e6e4169c37074052503cbc5
josellia <joseliaapires@hotmail.com>
2021-03-03 18:10:48 -0300

    refactor: refatorando importação módulos

--- like_singer/server/app.js
+++ like_singer/server/app.js
@@ -1,7 +1,9 @@
 import express  from 'express';
 import SingerRoutes from './routes/singer.routes';
-const cors = require('cors');
-const bodyParser = require("body-parser");
+import  cors  from 'cors';
+import bodyParser from "body-parser";
+import  multer from 'multer';
+import path from 'path';
 
 const app = express();
 
@@ -10,6 +12,11 @@ app.set('port', process.env.PORT ||  3000);
 app.use(express.json())
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}))
+
+// app.use(multer({
+//    dest:  
+// }).single('image'))
+
 app.use('/api/singers',SingerRoutes);
 
 export default app;
